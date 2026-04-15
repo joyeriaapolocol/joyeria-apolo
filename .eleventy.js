af13dd60e3306f467529788c9b72bf1df21a8f80
joyeriaapolocol <joyeriaapolo.col@gmail.com>
2026-04-15 14:47:03 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("img");
 
+  // 👇 ESTA ES LA QUE TE FALTA
+  eleventyConfig.addPassthroughCopy("admin");
+
   return {
     dir: {
       input: ".",
